e8ab23db6bd3e973a64ec51cfd1b32c1779aef00
o3m2a1r <103195436+o3m2a1r@users.noreply.github.com>
2026-02-13 11:53:06 +0300

    Create BookMetadata.cs

---
+++ AcmeLibrary/AcmeLibrary/Models/BookMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AcmeLibrary.Models
+{
+    [MetadataType(typeof(BookMetadata))]
+    public partial class book
+    {
+
+    }
+    public class BookMetadata
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public DateTime Published { get; set; }
+        public string Publisher { get; set; }
+    }
+}
